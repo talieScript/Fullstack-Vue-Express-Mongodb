4b087ac37cc3decbfbf7b70171406a1b7a65f63d
Taliesin <taliesin.bowes@hotmail.co.uk>
2019-05-19 19:08:33 +0100

    wrote working create and delete routes

--- sever/routes/api/posts.js
+++ sever/routes/api/posts.js
@@ -11,8 +11,21 @@ router.get("/", async (req, res) => {
 });
 
 // add posts
+router.post('/', async (req, res) => {
+    const posts = await loadPostCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
 
 // Delete post
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id) });
+    res.status(200).send();
+})
 
 
 async function loadPostCollection() {
