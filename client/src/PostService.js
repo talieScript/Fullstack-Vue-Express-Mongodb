7f40cc827025d6ccb6e39d12d77a484864781be8
Taliesin <taliesin.bowes@hotmail.co.uk>
2019-05-23 19:22:10 +0100

    created new vue app and set up post service with axios

---
+++ client/src/PostService.js
@@ -0,0 +1,37 @@
+import axios from axios;
+
+const url = 'http://localhost:8000/api/posts/';
+
+class PostService {
+    // Get Posts
+    static getPost() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
+                resolve(
+                    data.map(post => ({
+                        ...post,
+                        createdAt: new Date(post.createdAt)
+                    }))
+                );
+            } catch(err) {
+                reject(err);
+            }
+        })
+    }
+
+    // Create Post 
+    static insertPost(text) {
+        return axios.post(url, {
+            text
+        });
+    }
+
+    // delete Post
+    static deletePost(id) {
+        return axios.delete(`${url}${id}`)
+    }
+}
+
+export default PostService;
